7e93b3542969a9e04ef52a0714f2490eea9eb872
rc-stack <rc@before.health>
2026-06-29 13:25:43 -0400

    Add CI for SBOM

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,29 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.cyclonedx.model.Component
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.parcelize) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
+    id("org.cyclonedx.bom") version "3.2.4"
+}
+
+allprojects {
+    tasks.cyclonedxDirectBom {
+        // Opción recomendada: Usar listOf() en lugar de [...]
+        includeConfigs.set(listOf("releaseRuntimeClasspath"))
+        skipConfigs.set(listOf("androidTestRuntimeClasspath", "testRuntimeClasspath"))
+
+        projectType = Component.Type.APPLICATION
+        xmlOutput.unsetConvention()
+    }
+}
+
+tasks.cyclonedxBom {
+    projectType = Component.Type.APPLICATION
+    componentName = "Test Project"
+    componentVersion = "1.0.0"
+    includeBomSerialNumber = true
+    xmlOutput.unsetConvention()
 }
